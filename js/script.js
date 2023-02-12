2aa19e4763d1eea3caaf3cd257c452eed73c903a
zakaria bijaddiguene <zakariabijaddigune1234@gmail.com>
2023-02-13 00:36:36 +0100

    version 1.0.0

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 // SCROLL ANIMATION 
 AOS.init();
 
+// swipe titles
+const titles = document.querySelectorAll("#about_section .content h2")
+setInterval(() => {
+    titles[0].classList.toggle("hide")
+    titles[1].classList.toggle("hide")
+}, 4000);
+
 //NAVBAR 
 document.querySelector("#menu").onclick = () => {
     document.querySelector(".list").classList.add("show")
