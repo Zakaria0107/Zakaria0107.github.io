3077cea419d58dc0763b411f55a0e70af39b75d0
zakaria bijaddiguene <zakariabijaddigune1234@gmail.com>
2023-02-13 13:47:15 +0100

    add skills section

--- js/script.js
+++ js/script.js
@@ -15,3 +15,32 @@ document.querySelector("#menu").onclick = () => {
 document.querySelector("#hide-menu").onclick = () => {
     document.querySelector(".list").classList.remove("show")
 }
+
+
+window.onclick = () => {
+    document.querySelectorAll(".list a").forEach(elt => {
+        if(event.target == elt) {
+            document.querySelector(".list").classList.remove("show")
+        }
+    })
+}
+
+
+// FORM
+document.querySelector("#send").onclick = () => {
+    event.preventDefault()
+
+    let params = {
+        name : document.querySelector("#fullName") , 
+        email : document.querySelector("#email"), 
+        subject : document.querySelector("#subject"),
+        message : document.querySelector("#message")
+    }
+    
+    params.name.value = ""
+    params.email.value = ""
+    params.subject.value = ""
+    params.message.value = ""
+
+
+}
